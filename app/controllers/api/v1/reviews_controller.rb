2de2b7f33354d26c257e1ba033410c40ac91b466
simokh <m.simokhouili@gmail.com>
2021-04-21 16:36:45 -0400

    factoring

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -14,17 +14,16 @@ class Api::V1::ReviewsController < ApplicationController
 
 
     def create 
+        
         review = Review.new(strong_params)
-        # binding.pry
         if review.save
-        render json: ReviewSerializer.new(review), status: :accepted
+        render json: ReviewSerializer.new(review), status: :accepted 
         else
             render json: {errors: review.errors.full_messages}, status: :unprocessible_entity 
         end
     end
 
-    private
-
+    private 
 
     def strong_params
         params.require(:review).permit(:review_post, :rating, :museum_id)
